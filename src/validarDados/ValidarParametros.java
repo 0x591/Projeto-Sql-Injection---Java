eb25d36241e4e95f76d9cc0de036ed43b42e291a
0x591 <30768975+0x591@users.noreply.github.com>
2017-08-06 09:36:54 -0300

    Update ValidarParametros.java

--- src/validarDados/ValidarParametros.java
+++ src/validarDados/ValidarParametros.java
@@ -2,30 +2,27 @@
 package validarDados;
 
 import java.io.IOException;
-import sql.ControladorMain;
-import static sql.ControladorMain.RecebeUrl;
 import static sql.ControladorMain.adicionaUrl;
 
 public class ValidarParametros {
-    
+    // Método que recebe a url
     public ValidarParametros(String url) throws IOException {
         parseUrl(url);
     }
-    
+    // Método que separa os parametros
     public void parseUrl(String urlRecebe) throws IOException{
-        String url = urlRecebe;
-        char c = '&';
-        StringBuffer url2 = null;
+        String url = urlRecebe; // Salvando url na variavel
+        char simbolo = '&'; // Simbolo que vai ser procurado
+        StringBuffer url2 = null; 
         for (int i = 0; i < url.length(); i++) {            
-            url2 = new StringBuffer(url);
-            if (url.charAt(i) == c) {
-                int a = url.charAt(i);
-                url2.replace(i, i, "!-!");
-                adicionaUrl(url2.toString());                
+            url2 = new StringBuffer(url); // StringBuffer com a url
+            if (url.charAt(i) == simbolo) { // Verificando se tem o simbolo 
+                url2.replace(i, i, "!-!"); // Inserindo !-! na url 
+                adicionaUrl(url2.toString()); // Enviando a url para o teste
             }
-            if (i == url.length()-1){
-                url2.replace(i+1, i+1, "!-!");
-                adicionaUrl(url2.toString());           
+            if (i == url.length()-1){ // Verificando se o index chego no final
+                url2.replace(i+1, i+1, "!-!"); // Inserindo !-! no final da url
+                adicionaUrl(url2.toString()); // Enviando a url para o teste
             }            
         }
     }
